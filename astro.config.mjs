03d8d5a70068fa85af591db5cd5c58d37fff1f21
Bruce Denham <bdenham@adobe.com>
2023-09-11 15:18:09 -0500

    wip: dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,38 +6,46 @@ import overrideIntegration from "./src/overrideIntegration.mjs"; // import the i
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [
-    overrideIntegration(),
-    react(),
-    starlight({
-      title: 'Elsie SDK Docs',
-      social: {
-        github: 'https://github.com/commerce-docs/elsie.dev',
-      },
-      sidebar: [
-        { label: 'Quick start', link: '/start/start/' },
-        {
-          label: 'Components',
-          autogenerate: { directory: 'components' },
-        },
-        {
-          label: 'Design',
-          autogenerate: { directory: 'design' },
-        },
-
-        {
-          label: 'API',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-      customCss: ['./src/tailwind.css'],
-    }),
-    tailwind({ applyBaseStyles: true }),
-  ],
+  integrations: [overrideIntegration(), react(), starlight({
+    title: 'DropIn Kit',
+    logo: {
+      src: './src/assets/logo.svg',
+      replacesTitle: true,
+    },
+    social: {
+      github: 'https://github.com/commerce-docs/dropins.dev'
+    },
+    sidebar: [{
+      label: 'Quick start',
+      link: '/start/'
+    }, {
+      label: 'CLI usage',
+      link: '/cli-usage/'
+    }, {
+      label: 'Slots',
+      link: '/slots/'
+    }, {
+      label: 'Components',
+      autogenerate: {
+        directory: 'components'
+      }
+    }, {
+      label: 'Design',
+      autogenerate: {
+        directory: 'design'
+      }
+    }, {
+      label: 'API',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind()],
+  customCSS: './src/tailwind.css',
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp',
-    },
-  },
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
