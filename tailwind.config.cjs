52993561a4b3a8ebb26fa6df307a1196498afc26
Bruce Denham <bdenham@adobe.com>
2023-09-10 20:32:45 -0500

    wip: init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontSize: {
+        // Your preferred font scale. Starlight uses a 1.25 scale by default.
+        '2xs': '0.625rem',
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+      },
       colors: {
         // Your preferred accent color. Indigo is closest to Starlight’s defaults.
         accent: colors.blue,
