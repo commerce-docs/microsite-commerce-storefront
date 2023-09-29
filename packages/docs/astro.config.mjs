b0c8b4aae3e8a6bf0a1f4532b437b95c264fe857
Bruce Denham <bdenham@adobe.com>
2023-09-29 16:12:00 -0500

    fix: remove Elsie SDK from site

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -26,11 +26,11 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: 'DROPINS',
+          label: 'DropIns',
           items: [
             { label: 'About DropIns', link: '/dropins/about/' },
-            { label: 'Use DropIns', link: '/dropins/use/' },
-            { label: 'Customize DropIns', link: '/dropins/customize/' },
+            { label: 'Using DropIns', link: '/dropins/use/' },
+            { label: 'Customizing DropIns', link: '/dropins/customize/' },
             {
               label: 'Product Details DropIn',
               items: [
@@ -43,50 +43,6 @@ export default defineConfig({
               items: [
                 { label: 'About', link: '/dropins/checkout/about/' },
                 { label: 'API', link: '/dropins/checkout/api/' },
-              ],
-            },
-          ],
-        },
-        {
-          label: 'Elsie SDK',
-          collapsed: true,
-          items: [
-            { label: 'About', link: '/sdk/about/' },
-            { label: 'Quick start', link: '/sdk/start/' },
-            { label: 'CLI usage', link: '/sdk/cli/', },
-            { label: 'Slots', link: '/sdk/slots/', badge: 'New' },
-            { label: 'Components', 
-              items: [
-                { label: 'Overview', link: '/sdk/components/overview/' },
-              ],
-            },
-            {
-              label: 'Design',
-              items: [
-                { label: 'Overview', link: '/sdk/design/base/' },
-                { label: 'Colors', link: '/sdk/design/colors/' },
-                { label: 'Typography', link: '/sdk/design/typography/' },
-                { label: 'Spacing', link: '/sdk/design/spacing/' },
-                { label: 'Shapes', link: '/sdk/design/shapes/' },
-                { label: 'Grids', link: '/sdk/design/grid/' },
-              ],
-            },
-            {
-              label: 'Reference',
-              items: [
-                { label: 'Events', link: '/sdk/reference/events/' },
-                { label: 'Initialize', link: '/sdk/reference/initialize/' },
-                { label: 'Render', link: '/sdk/reference/render/' },
-              ],
-            },
-            {
-              label: 'Helpers',
-              items: [
-                { label: 'classList', link: '/sdk/helpers/classlist/' },
-                { label: 'debounce', link: '/sdk/helpers/debounce/' },
-                { label: 'deepmerge', link: '/sdk/helpers/deepmerge/' },
-                { label: 'getFormErrors', link: '/sdk/helpers/getformerrors/' },
-                { label: 'getFormValues', link: '/sdk/helpers/getformvalues/' },
               ],
             },
           ],
