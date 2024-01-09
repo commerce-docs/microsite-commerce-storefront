bf5cf393f2ffafd2cb491c7e3f58586127baf096
Bruce Denham <bdenham@adobe.com>
2024-01-08 19:02:54 -0600

    fix: lighthouse scores

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,6 @@ import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { text } from 'darkmatter';
 
 const i18n = defineCollection({ type: 'data', schema: i18nSchema() });
-// const docs = defineCollection({ type: 'content', schema: docsSchema() });
 const docs = defineCollection({
   type: 'content',
   schema: docsSchema({
