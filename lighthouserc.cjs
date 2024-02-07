2c457db6d50ecb2422213ae46865a9c1fbf00159
Bruce Denham <bdenham@adobe.com>
2024-02-07 17:28:13 -0600

    fix: add additional lighthouse settings

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,7 +1,24 @@
 module.exports = {
   ci: {
+    collect: {
+      numberOfRuns: 1,
+      settings: {
+        onlyCategories: ['performance'],
+        formFactor: 'desktop',
+      },
+    },
+    assert: {
+      // assert options here
+    },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'filesystem',
+      outputDir: '.lighthouseci',
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
     },
   },
 };
