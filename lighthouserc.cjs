b9a4323ccd5395f7d6c5d96f0a9f578f6726a46d
Bruce Denham <bdenham@adobe.com>
2024-02-07 18:41:59 -0600

    fix: final work on lighthouse ci

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,24 +1,25 @@
+const constants = require('./lighthouse-constants.cjs');
+
 module.exports = {
   ci: {
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
     collect: {
+      staticDistDir: './dist',
+      startServerCommand: 'npm run preview',
       numberOfRuns: 1,
+      staticDirFileDiscoveryDepth: 2,
+      maxAutodiscoverUrls: 0,
       settings: {
-        onlyCategories: ['performance'],
         formFactor: 'desktop',
+        throttling: constants.throttling.desktopDense4G,
+        screenEmulation: constants.screenEmulationMetrics.desktop,
+        emulatedUserAgent: constants.userAgents.desktop,
       },
     },
-    assert: {
-      // assert options here
-    },
     upload: {
-      target: 'filesystem',
-      outputDir: '.lighthouseci',
-    },
-    server: {
-      // server options here
-    },
-    wizard: {
-      // wizard options here
+      target: 'temporary-public-storage',
     },
   },
 };
