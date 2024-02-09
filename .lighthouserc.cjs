ef8c5411604b799fecd9debe3bb7e4944c3290ed
Bruce Denham <bdenham@adobe.com>
2024-02-09 13:09:45 -0600

    fix: update LH configs and clean up styles

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -136,10 +136,35 @@ const nonSimulatedSettingsOverrides = {
   cpuQuietThresholdMs: 5250,
 };
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-  throttling,
-  screenEmulationMetrics,
-  userAgents,
-  defaultSettings,
-  nonSimulatedSettingsOverrides,
+  ci: {
+    collect: {
+      staticDistDir: './dist',
+      numberOfRuns: 1,
+      staticDirFileDiscoveryDepth: 2,
+      maxAutodiscoverUrls: 0,
+      settings: {
+        formFactor: 'desktop',
+        throttling: throttling.desktopDense4G,
+        screenEmulation: screenEmulationMetrics.desktop,
+        emulatedUserAgent: userAgents.desktop,
+      },
+      url: [
+        'http://localhost/index.html',
+        'http://localhost/essentials/explore/index.html',
+        'http://localhost/essentials/install/index.html',
+        'http://localhost/essentials/connect/index.html',
+        'http://localhost/essentials/brand/index.html',
+        'http://localhost/essentials/customize/index.html',
+        'http://localhost/essentials/localize/index.html',
+      ],
+    },
+    upload: {
+      target: 'temporary-public-storage',
+      githubToken: process.env.LHCI_GITHUB_APP_TOKEN || '',
+    },
+  },
 };
