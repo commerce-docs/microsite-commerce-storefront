248ccd759b3c686ed24dde26b9676d513b3ab7eb
Bruce Denham <bdenham@adobe.com>
2024-02-12 11:41:06 -0600

    fix: check for issue before creating comment

--- .github/workflows/lighthouseScores.cjs
+++ .github/workflows/lighthouseScores.cjs
@@ -5,57 +5,58 @@ module.exports = ({ github, context }) => {
   const lighthouseReportPath = './.lighthouseci';
   const lighthouseScoresComment = generateLighthouseScoresComment(lighthouseReportPath);
 
-  return github.rest.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    body: lighthouseScoresComment,
-  });
-}
+  return context.issue.number
+    ? github.rest.issues.createComment({
+      issue_number: context.issue.number || 'no-issue-number',
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: lighthouseScoresComment,
+    })
+    : lighthouseScoresComment;
 
-function readJSONFile(filePath) {
-  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
-}
-
-function scoreToEmoji(score) {
-  return score === 100 ? '🎉' : score >= 90 ? '🟢' : score >= 70 ? '🟡' : '🔴';
-}
-
-function urlToPagePath(url) {
-  return new URL(url).pathname.substring(1);
-}
-
-function generateScoresForCategories(report) {
-  if (!report.categories || typeof report.categories !== 'object') {
-    return 'No categories found in report.\n';
+  function readJSONFile(filePath) {
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
   }
 
-  return Object.entries(report.categories).map(([_, category]) => {
-    const score = Math.round(category.score * 100);
-    const emoji = scoreToEmoji(score);
-    return `${emoji} **${category.title[0]}:** ${score}% `;
-  }).join('');
-}
+  function scoreToEmoji(score) {
+    return score === 100 ? '🎉' : score >= 90 ? '🟢' : score >= 70 ? '🟡' : '🔴';
+  }
 
-function generateLighthouseScoresComment(reportPath) {
-  let commentBody = '#### 🚀 Lighthouse Results\n';
+  function urlToPagePath(url) {
+    return new URL(url).pathname.substring(1);
+  }
 
-  const reportFiles = fs.readdirSync(reportPath);
-  reportFiles.forEach(file => {
-    if (file.includes('links') || path.extname(file) !== '.json') return;
-
-    const reportFilePath = path.join(reportPath, file);
-    const report = readJSONFile(reportFilePath);
-    const categoryScores = generateScoresForCategories(report);
-    commentBody += categoryScores;
-
-    if (report.requestedUrl) {
-      const pagePath = urlToPagePath(report.requestedUrl);
-      commentBody += `— ${pagePath} `;
+  function generateScoresForCategories(report) {
+    if (!report.categories || typeof report.categories !== 'object') {
+      return 'No categories found in report.\n';
     }
 
-    commentBody += '\n';
-  });
+    return Object.entries(report.categories).map(([_, category]) => {
+      const score = Math.round(category.score * 100);
+      const emoji = scoreToEmoji(score);
+      return `${emoji} **${category.title[0]}:** ${score}% `;
+    }).join('');
+  }
 
-  return commentBody;
-}
+  function generateLighthouseScoresComment(reportPath) {
+    let commentBody = '#### 🚀 Lighthouse Results\n';
+
+    const reportFiles = fs.readdirSync(reportPath);
+    reportFiles.forEach(file => {
+      if (file.includes('links') || path.extname(file) !== '.json') return;
+
+      const reportFilePath = path.join(reportPath, file);
+      const report = readJSONFile(reportFilePath);
+      const categoryScores = generateScoresForCategories(report);
+      commentBody += categoryScores;
+
+      if (report.requestedUrl) {
+        const pagePath = urlToPagePath(report.requestedUrl);
+        commentBody += `— ${pagePath} `;
+      }
+
+      commentBody += '\n';
+    });
+
+    return commentBody;
+  }
