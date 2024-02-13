18bd67c61eb8eb2025edd6f0934d7ef3ec5d661d
Bruce Denham <bdenham@adobe.com>
2024-02-13 12:50:52 -0600

    fix: styling overrides

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,103 +41,104 @@ export default defineConfig({
     social: {
       github: 'https://github.com/commerce-docs/dropins.dev'
     },
-    sidebar: [{
-      label: 'Getting Started',
-      badge: 'IP',
-      items: [{
-        label: 'Explore drop-ins',
-        link: '/essentials/explore/'
-      }, {
-        label: 'Install drop-ins',
-        link: '/essentials/install/'
-      }, {
-        label: 'Connect drop-ins',
-        link: '/essentials/connect/'
-      }, {
+    sidebar: [
+      {
+        label: 'Essentials',
         badge: 'IP',
-        label: 'Brand drop-ins',
-        link: '/essentials/brand/'
+        items: [{
+          label: 'Explore options',
+          link: '/essentials/explore/'
+        }, {
+          label: 'Install drop-ins',
+          link: '/essentials/install/'
+        }, {
+          label: 'Connect drop-ins',
+          link: '/essentials/connect/'
+        }, {
+          badge: 'IP',
+          label: 'Brand drop-ins',
+          link: '/essentials/brand/'
+        }, {
+          label: 'Localize drop-ins',
+          link: '/essentials/localize/'
+        }, {
+          label: 'Customize drop-ins',
+          link: '/essentials/customize/'
+        }]
       }, {
-        label: 'Localize drop-ins',
-        link: '/essentials/localize/'
+        label: 'Product Details',
+        badge: 'IP',
+        items: [{
+          label: 'Anatomy',
+          link: '/pdp/pdp-anatomy/'
+        }, {
+          label: 'Configure',
+          link: '/pdp/pdp-configure/'
+        }, {
+          label: 'Brand',
+          link: '/pdp/pdp-brand/'
+        }, {
+          label: 'Customize',
+          link: '/pdp/pdp-customize/'
+        }, {
+          label: 'Reference',
+          link: '/pdp/pdp-reference/'
+        }]
       }, {
-        label: 'Customize drop-ins',
-        link: '/essentials/customize/'
+        label: 'Cart',
+        badge: 'TBD',
+        collapsed: true,
+        items: [{
+          label: 'Anatomy',
+          link: '/cart/cart-anatomy/'
+        }, {
+          label: 'Configure',
+          link: '/cart/cart-configure/'
+        }, {
+          label: 'Brand',
+          link: '/cart/cart-brand/'
+        }, {
+          label: 'Customize',
+          link: '/cart/cart-customize/'
+        }, {
+          label: 'Reference',
+          link: '/cart/cart-reference/'
+        }]
+      }, {
+        label: 'Checkout',
+        badge: 'TBD',
+        collapsed: true,
+        items: [{
+          label: 'Anatomy',
+          link: '/checkout/checkout-anatomy/'
+        }, {
+          label: 'Configure',
+          link: '/checkout/checkout-configure/'
+        }, {
+          label: 'Brand',
+          link: '/checkout/checkout-brand/'
+        }, {
+          label: 'Customize',
+          link: '/checkout/checkout-customize/'
+        }, {
+          label: 'Reference',
+          link: '/checkout/checkout-reference/'
+        }]
+      }, {
+        label: 'Integrations',
+        badge: 'TBD',
+        collapsed: true,
+        items: [{
+          label: 'Edge Delivery',
+          link: '/integrate/integrate-eds/'
+        }, {
+          label: 'React',
+          link: '/integrate/integrate-react/'
+        }, {
+          label: 'Vue',
+          link: '/integrate/integrate-vue/'
+        }]
       }]
-    }, {
-      label: 'Product Details',
-      badge: 'IP',
-      items: [{
-        label: 'Anatomy',
-        link: '/pdp/pdp-anatomy/'
-      }, {
-        label: 'Configure',
-        link: '/pdp/pdp-configure/'
-      }, {
-        label: 'Brand',
-        link: '/pdp/pdp-brand/'
-      }, {
-        label: 'Customize',
-        link: '/pdp/pdp-customize/'
-      }, {
-        label: 'Reference',
-        link: '/pdp/pdp-reference/'
-      }]
-    }, {
-      label: 'Cart',
-      badge: 'TBD',
-      collapsed: true,
-      items: [{
-        label: 'Anatomy',
-        link: '/cart/cart-anatomy/'
-      }, {
-        label: 'Configure',
-        link: '/cart/cart-configure/'
-      }, {
-        label: 'Brand',
-        link: '/cart/cart-brand/'
-      }, {
-        label: 'Customize',
-        link: '/cart/cart-customize/'
-      }, {
-        label: 'Reference',
-        link: '/cart/cart-reference/'
-      }]
-    }, {
-      label: 'Checkout',
-      badge: 'TBD',
-      collapsed: true,
-      items: [{
-        label: 'Anatomy',
-        link: '/checkout/checkout-anatomy/'
-      }, {
-        label: 'Configure',
-        link: '/checkout/checkout-configure/'
-      }, {
-        label: 'Brand',
-        link: '/checkout/checkout-brand/'
-      }, {
-        label: 'Customize',
-        link: '/checkout/checkout-customize/'
-      }, {
-        label: 'Reference',
-        link: '/checkout/checkout-reference/'
-      }]
-    }, {
-      label: 'Integrations',
-      badge: 'TBD',
-      collapsed: true,
-      items: [{
-        label: 'Edge Delivery',
-        link: '/integrate/integrate-eds/'
-      }, {
-        label: 'React',
-        link: '/integrate/integrate-react/'
-      }, {
-        label: 'Vue',
-        link: '/integrate/integrate-vue/'
-      }]
-    }]
   }),],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
