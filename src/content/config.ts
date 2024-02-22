d42e31ddf025559f117f1d3a33b577ea53238938
Bruce Denham <bdenham@adobe.com>
2024-02-21 21:29:33 -0600

    fix: add prerequisite component and remove best practices

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,9 +2,24 @@ import { defineCollection, z as zod } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { text } from 'darkmatter';
 
-const prerequisiteSchema = zod.boolean().default(false).optional();
+const prerequisiteSchema = zod
+  .object({
+    html: zod.boolean().default(false).optional(),
+    css: zod.boolean().default(false).optional(),
+    js: zod.boolean().default(false).optional(),
+    github: zod.boolean().default(false).optional(),
+    sharepoint: zod.boolean().default(false).optional(),
+    googledrive: zod.boolean().default(false).optional(),
+    node: zod.boolean().default(false).optional(),
+    sidekick: zod.boolean().default(false).optional(),
+    codesync: zod.boolean().default(false).optional(),
+    commerce: zod.boolean().default(false).optional(),
+    graphql: zod.boolean().default(false).optional(),
+  })
+  .optional();
+
 const heroSchema = zod.object({ subtitle: zod.string() }).optional();
-const iframeSchema = zod.object({ src: zod.string() }).optional();
+const iframeSchema = zod.boolean().default(false).optional();
 
 const i18n = defineCollection({ type: 'data', schema: i18nSchema() });
 const docs = defineCollection({
@@ -14,20 +29,7 @@ const docs = defineCollection({
       description: text(),
       iframe: iframeSchema,
       hero: heroSchema,
-      prerequisites: zod
-        .object({
-          html: prerequisiteSchema,
-          css: prerequisiteSchema,
-          js: prerequisiteSchema,
-          github: prerequisiteSchema,
-          sharepoint: prerequisiteSchema,
-          googledrive: prerequisiteSchema,
-          node: prerequisiteSchema,
-          sidekick: prerequisiteSchema,
-          commerce: prerequisiteSchema,
-          graphql: prerequisiteSchema,
-        })
-        .optional(),
+      prerequisites: prerequisiteSchema.optional(),
     }),
   }),
 });
