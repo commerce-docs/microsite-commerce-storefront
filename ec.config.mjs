dad2d42b0e32f38060065120ae3c24f2a673d96e
Bruce Denham <bdenham@adobe.com>
2024-02-23 05:15:25 -0600

    fix: remove problematice collapse plugin until it is ready

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,13 +1,7 @@
-import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
+// import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
 // import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
 export default {
-  expressiveCode: {
-    plugins: [
-      pluginCollapsibleSections(),
-      // pluginLineNumbers(),
-    ],
-  },
   themes: ['dark-plus', 'github-light'],
   styleOverrides: {
     codePaddingInline: '1rem',
