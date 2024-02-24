9abb8add11969bf702acbf516dac5443331acbe9
Bruce Denham <bdenham@adobe.com>
2024-02-23 20:08:21 -0600

    fix: Added features to CodeImport component and cleanup

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,8 +1,18 @@
-// import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
-// import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
+/** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
+  plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
   themes: ['dark-plus', 'github-light'],
+  defaultProps: {
+    showLineNumbers: false,
+    overridesByLang: {
+      'js,html': {
+        showLineNumbers: false,
+      },
+    },
+  },
   styleOverrides: {
     codePaddingInline: '1rem',
     codePaddingBlock: '1rem',
