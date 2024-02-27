17e517291cf03b53ccfa5395816e0cdc2f92e9a2
Bruce Denham <bdenham@adobe.com>
2024-02-27 11:33:27 -0600

    fix: organize clean styles icons an components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,6 @@ module.exports = {
   content: ['./src/**/*.{astro,html,css,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        accent: colors.slate,
-        gray: colors.neutral,
-      },
       fontFamily: {
         sans: ["'Adobe Clean', adobe-clean, sans-serif"],
         mono: ["'Courier', monospace"],
