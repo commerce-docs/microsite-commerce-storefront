6891b788aadda0ce268d29ef4e79a9ec8e879f12
Bruce Denham <bdenham@adobe.com>
2024-03-21 12:25:19 -0500

    fix: remove qwik integrations temporarily to fix microsite

--- src/components/StackBlitz.tsx
+++ src/components/StackBlitz.tsx
@@ -1,43 +1,43 @@
-import { component$, useVisibleTask$ } from '@builder.io/qwik';
-import type { UiThemeOption, UiViewOption } from '@stackblitz/sdk';
+// // import { component$, useVisibleTask$ } from '@builder.io/qwik';
+// import type { UiThemeOption, UiViewOption } from '@stackblitz/sdk';
 
-interface Props {
-  id: string;
-  file: string;
-  view?: 'editor' | 'preview' | null;
-  hideExplorer?: boolean;
-  height?: number;
-  theme?: 'dark' | 'light' | 'default' | null;
-  clickToLoad?: boolean;
-}
+// interface Props {
+//   id: string;
+//   file: string;
+//   view?: 'editor' | 'preview' | null;
+//   hideExplorer?: boolean;
+//   height?: number;
+//   theme?: 'dark' | 'light' | 'default' | null;
+//   clickToLoad?: boolean;
+// }
 
-const StackBlitz = component$<Props>(
-  ({
-    id,
-    file,
-    view = 'default',
-    hideExplorer = true,
-    height = 500,
-    theme = 'dark',
-    clickToLoad = true,
-  }) => {
-    useVisibleTask$(async () => {
-      const sdk = (await import('@stackblitz/sdk')).default;
+// const StackBlitz = component$<Props>(
+//   ({
+//     id,
+//     file,
+//     view = 'default',
+//     hideExplorer = true,
+//     height = 500,
+//     theme = 'dark',
+//     clickToLoad = true,
+//   }) => {
+//     useVisibleTask$(async () => {
+//       const sdk = (await import('@stackblitz/sdk')).default;
 
-      sdk.embedProjectId('stackblitz-embed', id, {
-        forceEmbedLayout: true,
-        openFile: file,
-        view: view as UiViewOption,
-        hideExplorer: hideExplorer,
-        hideNavigation: false,
-        height: height,
-        theme: theme as UiThemeOption,
-        clickToLoad: clickToLoad,
-      });
-    });
+//       sdk.embedProjectId('stackblitz-embed', id, {
+//         forceEmbedLayout: true,
+//         openFile: file,
+//         view: view as UiViewOption,
+//         hideExplorer: hideExplorer,
+//         hideNavigation: false,
+//         height: height,
+//         theme: theme as UiThemeOption,
+//         clickToLoad: clickToLoad,
+//       });
+//     });
 
-    return <div id="stackblitz-embed" style="border: 1px solid #ccc;" />;
-  }
-);
+//     return <div id="stackblitz-embed" style="border: 1px solid #ccc;" />;
+//   }
+// );
 
-export default StackBlitz;
+// export default StackBlitz;
