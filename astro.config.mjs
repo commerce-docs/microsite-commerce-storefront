1751dbade49a00a55115bf21188ee255710b1ba5
Bruce Denham <bdenham@adobe.com>
2024-03-29 15:14:00 -0500

    feat: add Google verification meta tag\n\nAdded a meta tag in the head section of the website to allow Google to verify ownership of the site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,13 @@ export default defineConfig({
       attrs: {
         src: 'https://assets.adobedtm.com/a7d65461e54e/6e9802a06173/launch-43baf8381f4b.min.js'
       }
+    },
+    {
+      tag: 'meta',
+      attrs: {
+        name: 'google-site-verification',
+        content: 'NwoVbL9MrtJAa4vdfMC0vJmKV3Hvuc4L_UHlv4Uzjgk'
+      }
     }],
     title: 'Adobe Commerce Storefront',
     favicon: 'favicon.ico',
