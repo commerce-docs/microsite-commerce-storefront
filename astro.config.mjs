4860c05d79ad9741f2ec21978d2c4a36a1cb68c2
Bruce Denham <bdenham@adobe.com>
2024-09-03 11:28:29 -0500

    remove anatomy topic, include in features topic

--- astro.config.mjs
+++ astro.config.mjs
@@ -122,13 +122,13 @@ export default defineConfig({
             link: '/dropins/all/enriching/'
           },
           {
-            label: 'Dropin Template',
+            label: 'DROPIN_NAME',
             collapsed: false,
             items: [
               { label: 'DROPIN_NAME features', link: '/dropins/blueprint/dropin-features/' },
               { label: 'DROPIN_NAME prerequisites', link: '/dropins/blueprint/dropin-prerequisites/' },
-              { label: 'DROPIN_NAME anatomy', link: '/dropins/blueprint/dropin-anatomy/' },
-              { label: 'DROPIN_NAME configurations', link: '/dropins/blueprint/dropin-configurations/' },
+              { label: 'DROPIN_NAME initialization', link: '/dropins/blueprint/dropin-initialization/' },
+              { label: 'DROPIN_NAME containers', link: '/dropins/blueprint/dropin-containers/' },
               { label: 'DROPIN_NAME styles', link: '/dropins/blueprint/dropin-styles/' },
               { label: 'DROPIN_NAME slots', link: '/dropins/blueprint/dropin-slots/' },
               { label: 'DROPIN_NAME functions', link: '/dropins/blueprint/dropin-functions/' },
