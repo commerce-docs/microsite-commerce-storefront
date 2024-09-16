d2ec1d465d0953edcb5ad8945132111c6a34f619
Jeff Matthews <jmatthew@adobe.com>
2024-09-16 16:33:15 -0500

    USF-1327: Added stub files

--- astro.config.mjs
+++ astro.config.mjs
@@ -153,7 +153,7 @@ export default defineConfig({
               { label: 'Overview', link: '/dropins/checkout/' },
               // { label: 'Checkout Installation', link: '/dropins/checkout/checkout-installation/' },
               // { label: 'Checkout Styles', link: '/dropins/checkout/checkout-styles/' },
-              // { label: 'Checkout Containers', link: '/dropins/checkout/checkout-containers/' },
+              { label: 'Checkout Containers', link: '/dropins/checkout/checkout-containers/' },
               // { label: 'Checkout Slots', link: '/dropins/checkout/checkout-slots/' },
               // { label: 'Checkout Functions', link: '/dropins/checkout/checkout-functions/' },
             ]
