6218d5d6265479aa347cc5fbaa1b6c7e975e0b99
Kevin Harper <keharper@adobe.com>
2024-11-13 14:05:14 -0600

    More developer review comments

--- src/content/docs/dropins/cart/_includes/cartModel.ts
+++ src/content/docs/dropins/cart/_includes/cartModel.ts
@@ -1,4 +1,4 @@
-type CartModel {
+export interface CartModel {
   id: string;
   totalQuantity: number;
   errors?: ItemError[];
@@ -30,4 +30,93 @@ type CartModel {
   hasOutOfStockItems?: boolean;
   hasFullyOutOfStockItems?: boolean;
   appliedCoupons?: Coupon[];
+}
+
+interface TotalPriceModifier {
+  amount: Price;
+  label: string;
+  coupon?: Coupon;
+}
+
+interface FixedProductTax {
+  amount: Price;
+  label: string;
+}
+
+export interface Item {
+  taxedPrice: Price;
+  rowTotal: Price;
+  rowTotalIncludingTax: Price;
+  itemType: string;
+  uid: string;
+  url: ItemURL;
+  quantity: number;
+  sku: string;
+  name: string;
+  image: ItemImage;
+  links?: ItemLinks;
+  price: Price;
+  total: Price;
+  discountedTotal?: Price;
+  discount?: Price;
+  regularPrice: Price;
+  discounted: boolean;
+  bundleOptions?: { [key: string]: any };
+  selectedOptions?: { [key: string]: any };
+  customizableOptions?: { [key: string]: any };
+  message?: string;
+  recipient?: string;
+  recipientEmail?: string;
+  sender?: string;
+  senderEmail?: string;
+  lowInventory?: boolean;
+  insufficientQuantity?: boolean;
+  onlyXLeftInStock?: number | null;
+  outOfStock?: boolean;
+  notAvailableMessage?: string;
+  stockLevel?: String;
+  discountPercentage?: number;
+  savingsAmount?: Price;
+  productAttributes?: Attribute[];
+  fixedProductTaxes?: FixedProductTax[];
+}
+
+interface ItemError {
+  id: string;
+  text: string;
+}
+
+interface ItemImage {
+  src: string;
+  alt: string;
+}
+
+export interface Price {
+  value: number;
+  currency: string;
+}
+
+interface ItemURL {
+  urlKey: string;
+  categories: string[];
+}
+
+interface ItemLinks {
+  count: number;
+  result: string;
+}
+
+interface AttributeOption {
+  value: string;
+  label: string;
+}
+
+interface Attribute {
+  code: string;
+  value?: string;
+  selected_options?: AttributeOption[];
+}
+
+interface Coupon {
+  code: string;
 }
