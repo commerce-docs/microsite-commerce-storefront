3a6f5ba367e7348f2b8a1ef41da0e62cb316c097
Kevin Harper <keharper@adobe.com>
2024-12-09 12:57:43 -0600

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -232,6 +232,7 @@ export default defineConfig({
                 collapsed: true,
                 items: [
                   { label: 'Configure the cart summary block', link: '/dropins/cart/tutorials/configure-cart-summary/' },
+                  { label: 'Configure the order summary block', link: '/dropins/cart/tutorials/order-summary/' },
                   { label: 'Order summary lines', link: '/dropins/cart/tutorials/order-summary-lines/' },
                 ]
               },
