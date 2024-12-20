6615ddbc8f9fe9f895643dfdaeec94017a9119b5
bdenham <bdenham@adobe.com>
2024-12-20 07:26:39 -0600

    fix: update inaccurate correction

--- _dropin-templates/SidebarConfiguration.mjs
+++ _dropin-templates/SidebarConfiguration.mjs
@@ -6,7 +6,7 @@
   items: [
     { label: 'DROPIN_NAME anatomy', link: '/dropins/blueprint/dropin-anatomy/' },
     { label: 'DROPIN_NAME prerequisites', link: '/dropins/blueprint/dropin-prerequisites/' },
-    { label: 'DROPIN_NAME initialization', link: '/dropins/blueprint/drop-initialization/' },
+    { label: 'DROPIN_NAME initialization', link: '/dropins/blueprint/dropin-initialization/' },
     { label: 'DROPIN_NAME containers', link: '/dropins/blueprint/dropin-containers/' },
     { label: 'DROPIN_NAME styles', link: '/dropins/blueprint/dropin-styles/' },
     { label: 'DROPIN_NAME slots', link: '/dropins/blueprint/dropin-slots/' },
