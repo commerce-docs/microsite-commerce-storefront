1559d00ff3b1bb285b30c2713d69504d37e81c1e
bdenham <brucedenham@mac.com>
2025-03-02 15:53:16 -0500

    feat: added MDX content and sidebar pattern to pages to support top-level nav

---
+++ src/pages/merchants/sidebar.js
@@ -0,0 +1,9 @@
+export const sidebar = [
+  {
+    label: 'Merchants',
+    items: [
+      { label: 'Overview', link: '/merchants/' },
+      { label: 'Multi-store setup', link: '/merchants/multistore/' },
+    ],
+  },
+];
