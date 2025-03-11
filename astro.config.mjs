367c7f2102c1ec43453602bf65eab01a81702df6
Kevin Harper <keharper@adobe.com>
2025-03-11 13:21:25 -0500

    Add gift options/gift card docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -277,6 +277,7 @@ export default defineConfig({
                   { label: 'EmptyCart', link: '/dropins/cart/containers/empty-cart/' },
                   { label: 'EstimateShipping', link: '/dropins/cart/containers/estimate-shipping/' },
                   { label: 'GiftCards', link: '/dropins/cart/containers/gift-cards/' },
+                  { label: 'GiftOptions', link: '/dropins/cart/containers/gift-options/' },                  
                   { label: 'MiniCart', link: '/dropins/cart/containers/minicart/' },
                   { label: 'OrderSummary', link: '/dropins/cart/containers/order-summary/' },
                   { label: 'OrderSummaryLine', link: '/dropins/cart/containers/order-summary-line/' },
