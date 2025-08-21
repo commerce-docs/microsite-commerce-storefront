b06ec4d5a51e85a9e544e553dfd8994a1529ec3f
Bruce Denham <bdenham@adobe.com>
2025-08-21 10:11:16 -0500

    wip: playground query fixes

--- src/components/Graphiql/queries/endpoint.js
+++ src/components/Graphiql/queries/endpoint.js
@@ -2,7 +2,7 @@ export const ACCS_ENDPOINT =
   'https://edge-graph.adobe.io/api/489c8449-21a0-4155-9f95-7608d36970d6/graphql';
 
 export const ACO_ENDPOINT =
-  'https://na1-qa.api.commerce.adobe.com/LnjXjuam6uWmo5d2JLEgbA/graphql';
+  'https://na1-sandbox.api.commerce.adobe.com/Fwus6kdpvYCmeEdcCX7PZg/graphql';
 
 export const getEndpoint = (service) => {
   if (service === 'ACO') {
