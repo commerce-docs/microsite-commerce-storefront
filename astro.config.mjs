557428b4dacee3c9e958e5ebd55a16e1c29257a5
Bruce Denham <bdenham@adobe.com>
2025-11-02 17:24:50 -0600

    Remove common-events sidebar entry (belongs in documentation PR)

--- astro.config.mjs
+++ astro.config.mjs
@@ -338,7 +338,6 @@ async function config() {
                       { label: 'Slots', link: '/dropins/all/slots/' },
                       { label: 'Layouts', link: '/dropins/all/layouts/' },
                       { label: 'Events', link: '/dropins/all/events/' },
-                      { label: 'Common events', link: '/dropins/all/common-events/' },
                       { label: 'Extending', link: '/dropins/all/extending/' },
                       { label: 'Creating', link: '/dropins/all/creating/' },
                     ],
