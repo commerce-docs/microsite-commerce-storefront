f228731de8c5f632530711bf0eeee33e6129576a
Bruce Denham <bdenham@adobe.com>
2026-06-22 16:36:53 -0500

    updates to SCP packages

--- astro.sidebar.mjs
+++ astro.sidebar.mjs
@@ -158,9 +158,6 @@ export function generateSidebar() {
               items: [
                 { label: 'Installation', link: '/setup/configuration/storefront-compatibility/install/' },
                 { label: 'Adobe Commerce 2.4.9', link: '/setup/configuration/storefront-compatibility/v249/' },
-                { label: 'Adobe Commerce 2.4.8', link: '/setup/configuration/storefront-compatibility/v248/' },
-                { label: 'Adobe Commerce 2.4.7', link: '/setup/configuration/storefront-compatibility/v247/' },
-                { label: 'B2B Compatibility Package', link: '/setup/configuration/storefront-compatibility-b2b/' },
               ],
             },
             {
