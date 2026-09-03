80b69c7196d131fc9a8238d8053234f1d9736691
Bruce Denham <bdenham@adobe.com>
2026-09-03 10:10:03 -0500

    Revert "Merge pull request #1018 from commerce-docs/ACCS-1665-sfl-docs"
    This reverts commit a00e8906ed0ed9011f6bbd614a14bcff5ac406f8, reversing
    changes made to 689221472a2e1f039dcc660b3a362dacbba344d2.

--- astro.sidebar.mjs
+++ astro.sidebar.mjs
@@ -533,13 +533,6 @@ export function generateSidebar() {
               items: [
                 { label: 'Overview', link: '/dropins/wishlist/' },
                 { label: 'Quick Start', link: '/dropins/wishlist/quick-start/' },
-                {
-                  label: 'How-Tos',
-                  collapsed: true,
-                  items: [
-                    { label: 'Render an Additional Wishlist', link: '/dropins/wishlist/tutorials/render-additional-wishlist/' },
-                  ],
-                },
                 { label: 'Initialization', link: '/dropins/wishlist/initialization/' },
                 { label: 'Functions', link: '/dropins/wishlist/functions/' },
                 { label: 'Slots', link: '/dropins/wishlist/slots/' },
